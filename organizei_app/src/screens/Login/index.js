dfc3a935f665e210ba1cbcb200cfc8e416a7d73d
Rafael <rafael.jcunha@hotmail.com>
2020-10-17 18:18:15 -0300

    Loading apos o Cadastro

--- organizei_app/src/screens/Login/index.js
+++ organizei_app/src/screens/Login/index.js
@@ -47,7 +47,7 @@ export default function Login({navigation}) {
                 style={styles.button}
                 title="Comece a organizar"
                 textStyle={styles.textStyle}
-                onClick={() => console.log('clicou entrou')}
+                onClick={() => navigation.navigate('MainEntrance')}
                 opacity={0.1}
               />
             </View>
